9fa002db7a86ccdd693c4371e437b1da949dffef
Svitlana Pushchalo <svitlana8@gmail.com>
2021-07-11 21:48:02 +0300

    Adding results for HW#5 tasks 3-4: staticmethod_classmethod

--- homeworks/oop_2/HW#5_OOP2_SvitlanaPushchalo.py
+++ homeworks/oop_2/HW#5_OOP2_SvitlanaPushchalo.py
@@ -45,3 +45,56 @@ gs = GuitarString()
 guitar = Guitar(gs)
 print(guitar.string)
 # OUTPUT: <__main__.GuitarString object at 0x7f398ed32070>
+
+
+print('\n########## 3 ##########')
+# class Calc:
+#     """
+#     Make class with one method "add_nums" with 3 parameters, which returns sum of these parameters.
+#     Note: this method should be static
+#     """
+
+class Calc:
+
+    @staticmethod
+    def add_nums(a, b, c):
+        summa = a + b + c
+        return summa
+
+addnums = Calc()
+print(addnums.add_nums(1, 2, 3))
+# OUTPUT: 6
+print(Calc.add_nums(1, 2, 3))
+# OUTPUT: 6
+
+
+print('\n########## 4* ##########')
+# class Pasta:
+#     """
+#     Make class which takes 1 parameter on init - list of ingredients and defines instance attribute ingredients.
+#     It should have 2 methods:
+#     carbonara (['forcemeat', 'tomatoes']) and bolognaise (['bacon', 'parmesan', 'eggs'])
+#     which should create Pasta instances with predefined list of ingredients.
+#     Example:
+#         pasta_1 = Pasta(["tomato", "cucumber"])
+#         pasta_1.ingredients will equal to ["tomato", "cucumber"]
+#         pasta_2 = Pasta.bolognaise()
+#         pasta_2.ingredients will equal to ['bacon', 'parmesan', 'eggs']
+#     """
+
+class Pizza:
+    def __init__(self, ingredients = []):
+        self.ingredients = ingredients
+
+    @classmethod
+    def carbonara (cls):
+        return Pizza(['forcemeat', 'tomatoes'])
+
+    @classmethod
+    def bolognaise (cls):
+        return Pizza(['bacon', 'parmesan', 'eggs'])
+
+pizza_carbonara = Pizza.carbonara()
+print('pizza_carbonara:', pizza_carbonara.ingredients)
+pizza_bolognaise = Pizza.bolognaise()
+print('pizza_bolognaise:', pizza_bolognaise.ingredients)
