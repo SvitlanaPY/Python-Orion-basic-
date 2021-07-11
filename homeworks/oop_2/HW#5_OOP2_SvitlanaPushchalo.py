fa75958acc30f626ecc71149e1ca8fe5a17db043
Svitlana Pushchalo <svitlana8@gmail.com>
2021-07-11 21:52:30 +0300

    Adding results for HW#5 tasks 6-7: DataClass_NamedTuple

--- homeworks/oop_2/HW#5_OOP2_SvitlanaPushchalo.py
+++ homeworks/oop_2/HW#5_OOP2_SvitlanaPushchalo.py
@@ -136,3 +136,49 @@ concert = Concert()
 concert.visitors_count = 10
 print(concert.visitors_count)
 # OUTPUT: 10
+
+
+print('\n########## 6 ##########')
+# class AddressBookDataClass:
+#     """
+#     Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
+#     """
+
+from dataclasses import dataclass
+@dataclass
+class AddressBookDataClass:
+    key: int
+    name: str
+    phone_number: str
+    address: str
+    email: str
+    birthday: str
+    age: int
+
+address_book = AddressBookDataClass(101,
+                               'John Smith',
+                               '(888) 558-9004',
+                               '123 Main St, Pasadena, CA',
+                               ' jsmith@comcast.net',
+                               '01 January 2000',
+                               21)
+print(address_book)
+# OUTPUT: AddressBookDataClass(key=10, name='John Smith', phone_number='(888) 558-9004', address='123 Main St, Pasadena, CA', email=' jsmith@comcast.net', birthday='01 January 2000', age=21)
+print(address_book.name)
+# OUTPUT: John Smith
+
+
+print('\n########## 7 ##########')
+# Create the same class (6) but using NamedTuple
+
+from collections import namedtuple
+AddressBookNamedTuple = namedtuple('AddressBookNamedTuple', ['key', 'name', 'phone_number', 'address', 'email', 'birthday', 'age'])
+ab = AddressBookNamedTuple('10', 'John Smith', '(888) 558-9004', '123 Main St, Pasadena, CA', ' jsmith@comcast.net', '01 January 2000', '21')
+print(ab)
+# OUTPUT: AddressBookNamedTuple(key='10', name='John Smith', phone_number='(888) 558-9004', address='123 Main St, Pasadena, CA', email=' jsmith@comcast.net', birthday='01 January 2000', age='21')
+print(ab[0:3])
+# OUTPUT: ('10', 'John Smith', '(888) 558-9004')
+print('address:', ab.address)
+# OUTPUT: address: 123 Main St, Pasadena, CA
+
+
