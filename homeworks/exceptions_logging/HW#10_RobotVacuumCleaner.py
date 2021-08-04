217601cb862edd3eb851fa190105d0431c307e82
Svitlana Pushchalo <svitlana8@gmail.com>
2021-08-04 17:25:14 +0300

    Adding complete HW#10_RobotCleaner

--- homeworks/exceptions_logging/HW#10_RobotVacuumCleaner.py
+++ homeworks/exceptions_logging/HW#10_RobotVacuumCleaner.py
@@ -5,11 +5,14 @@ from random import randint
 class NoWater(Exception):
     pass
 
+
 class FullTrashBin(Exception):
     pass
 
-class ChargeLower20(Exception):
+
+class BatteryChargeLower20(Exception):
     pass
+
 
 class NoPower(Exception):
     pass
@@ -24,33 +27,40 @@ class Robot:
         self.trash = trash
         self.water = water
 
-
     def move(self):
         while True:
             time.sleep(1)
-            print("MOVE")
-            self.battery_charge = self.battery_charge - Robot.BATTERY_REDUCE
-            self.wash()
-            self.vacuum_cleaner()
-
+            if self.battery_charge > 0:
+                self.battery_charge -= Robot.BATTERY_REDUCE
+                print("MOVE")
+                self.wash()
+                self.vacuum_cleaner()
+            try:
+                if self.battery_charge <= 0:
+                    raise NoPower
+                if self.battery_charge < 20:
+                    raise BatteryChargeLower20
+            except BatteryChargeLower20:
+                print("Battery charge is lower than 20")
+            except NoPower:
+                print("NO power! Charge me, please!")
 
     def wash(self):
         try:
             if self.water <= 0:
                 raise NoWater
             else:
-                self.water = self.water - Robot.WATER_REDUCE
+                self.water -= Robot.WATER_REDUCE
                 print("WASH")
         except NoWater:
             print("NO water! Fill in water tank, please!")
 
-
     def vacuum_cleaner(self):
         try:
-            if self.trash >= 99:
+            if self.trash >= 100:
                 raise FullTrashBin
             else:
-                self.trash = self.trash + randint(0, 10)
+                self.trash += randint(0, 10)
                 print("VACUUM CLEANING")
         except FullTrashBin:
             print("Full trash bin! Clean out, please!")
